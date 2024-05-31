8cbbcb72d5cd797a193b9b561df95a0ab73d179c
Nabil <nabil.ziani@brightest.be>
2024-05-31 18:44:12 +0200

    update types

--- app/(root)/(home)/enrollment/[id]/page.tsx
+++ app/(root)/(home)/enrollment/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Divider } from '@nextui-org/react'
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import EnrollmentForm from '@/components/EnrollmentForm';
+import { capitalize } from '@/lib/utils';
 
 const EnrollmentPage = async ({ params: { id } }: { params: { id: string } }) => {
     const supabase = createClient();
@@ -51,11 +52,12 @@ const EnrollmentPage = async ({ params: { id } }: { params: { id: string } }) =>
     }
 
     const enrollment = await getCompleteEnrollment()
+    const student = enrollment?.data![0].student
 
     return (
         <>
             <div className="flex justify-between items-center">
-                <h1 className='text-3xl font-bold'>Nieuwe inschrijving</h1>
+                <h1 className='text-3xl font-bold'>{student ? `Inschrijving ${capitalize(student.firstname)} ${capitalize(student.lastname)}` : 'Nieuwe inschrijving'}</h1>
                 {/* <Button>Some action</Button> */}
             </div>
             <Divider className="my-5" />
--- utils/database.types.ts
+++ utils/database.types.ts
@@ -118,11 +118,13 @@ export type Database = {
           email_2: string | null
           firstname: string
           gender: Database["public"]["Enums"]["gender"]
+          homeAlone: boolean
           housenumber: string | null
           lastname: string
           phone_1: string
           phone_2: string | null
           postalcode: string | null
+          remarks: string | null
           street: string | null
           studentid: string
         }
@@ -133,11 +135,13 @@ export type Database = {
           email_2?: string | null
           firstname: string
           gender: Database["public"]["Enums"]["gender"]
+          homeAlone?: boolean
           housenumber?: string | null
           lastname: string
           phone_1: string
           phone_2?: string | null
           postalcode?: string | null
+          remarks?: string | null
           street?: string | null
           studentid?: string
         }
@@ -148,11 +152,13 @@ export type Database = {
           email_2?: string | null
           firstname?: string
           gender?: Database["public"]["Enums"]["gender"]
+          homeAlone?: boolean
           housenumber?: string | null
           lastname?: string
           phone_1?: string
           phone_2?: string | null
           postalcode?: string | null
+          remarks?: string | null
           street?: string | null
           studentid?: string
         }
@@ -169,22 +175,22 @@ export type Database = {
       classtime: "VM" | "MD" | "NM"
       classtype: "Woensdag" | "Zondag" | "Weekend"
       enrollment_status:
-      | "Heringeschreven"
-      | "Niet ingeschreven"
-      | "Onder voorbehoud"
+        | "Heringeschreven"
+        | "Niet ingeschreven"
+        | "Onder voorbehoud"
       enrollmentstatus:
-      | "Heringeschreven"
-      | "Niet ingeschreven"
-      | "Onder voorbehoud"
+        | "Heringeschreven"
+        | "Niet ingeschreven"
+        | "Onder voorbehoud"
       gender: "m" | "f"
       leveltype:
-      | "Tamhiedi"
-      | "Niveau 1"
-      | "Niveau 2"
-      | "Niveau 3 - deel 1"
-      | "Niveau 3 - deel 2"
-      | "Niveau 4 - deel 1"
-      | "Niveau 4 - deel 2"
+        | "Tamhiedi"
+        | "Niveau 1"
+        | "Niveau 2"
+        | "Niveau 3 - deel 1"
+        | "Niveau 3 - deel 2"
+        | "Niveau 4 - deel 1"
+        | "Niveau 4 - deel 2"
     }
     CompositeTypes: {
       [_ in never]: never
@@ -196,80 +202,80 @@ type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
-  | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
-  | { schema: keyof Database },
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-  ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-    Database[PublicTableNameOrOptions["schema"]]["Views"])
-  : never = never,
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-    Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
-  ? R
-  : never
+    ? R
+    : never
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-    PublicSchema["Views"])
-  ? (PublicSchema["Tables"] &
-    PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-  ? R
-  : never
-  : never
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-  | keyof PublicSchema["Tables"]
-  | { schema: keyof Database },
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-  ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-  : never = never,
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-    Insert: infer I
-  }
-  ? I
-  : never
+      Insert: infer I
+    }
+    ? I
+    : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-    Insert: infer I
-  }
-  ? I
-  : never
-  : never
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-  | keyof PublicSchema["Tables"]
-  | { schema: keyof Database },
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-  ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-  : never = never,
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-    Update: infer U
-  }
-  ? U
-  : never
+      Update: infer U
+    }
+    ? U
+    : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-    Update: infer U
-  }
-  ? U
-  : never
-  : never
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
-  | keyof PublicSchema["Enums"]
-  | { schema: keyof Database },
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-  : never = never,
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-  : never
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
