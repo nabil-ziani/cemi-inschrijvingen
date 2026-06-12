2303dbc3eadf30729499c5c678cc7ec86d336aaf
Nabil <nabil_ee@hotmail.com>
2026-06-12 09:37:25 +0200

    fix new enrollment

--- app/(root)/(home)/enrollment/[id]/page.tsx
+++ app/(root)/(home)/enrollment/[id]/page.tsx
@@ -13,7 +13,7 @@ interface EnrollmentPageProps {
 
 const EnrollmentPage = async ({ params }: EnrollmentPageProps) => {
 	const { id } = await params;
-	
+
 	const supabase = await createClient();
 
 	// --- Auth protect page ---
@@ -30,7 +30,7 @@ const EnrollmentPage = async ({ params }: EnrollmentPageProps) => {
 	// This will always be from year 2025, because user comes from table where only 2025-records are shown
 	const getCurrentEnrollment = async (): Promise<EnrollmentWithStudentClass | null> => {
 		// ID will be null when student is new (no 2025-enrollment)
-		if (!id) {
+		if (id === 'null') {
 			return null
 		} else {
 			const { data, error: enrollmentError } = await supabase.from('enrollment').select(`*, student(*), class(*, level(*))`).eq('enrollmentid', id).limit(1).single()
